1fe3d188c233ad0538dc49dc2af96e5aab2eb1f8
unknown <khaledabulibdeh@gmail.com>
2022-04-04 21:27:05 +0300

    added order.js

--- order/order.js
+++ order/order.js
@@ -1,1 +1,24 @@
 const express = require("express")
+const axios = require("axios")
+const app = express()
+const port = 3003
+const catalogServer = "http://localhost:3002"
+
+app.get('/purchase/:id', async (req, res) => {
+  try {
+    // Get the quantity for the book Id
+    const bookId = req.params.id;
+    const bookResponse = await axios.get(catalogServer + `/quantity/${bookId}`)
+    const bookQuantity = bookResponse.data.quantity
+
+    // If the quantity is greater than 0, purchase the book, otherwise return a {success: false} json message
+    const success = (bookQuantity > 0) ? await axios.put(catalogServer + `/decrement/${bookId}`) : {data: {success: false}}
+    res.json(success.data)
+  } catch (err) {
+    console.log(err)
+  }
+})
+
+app.listen(port, () => {
+  console.log("Order Server is Running!")
+})
